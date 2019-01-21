2425d11850e43fed36c36d60fa45c72157b36315
Chantelle Layton <layton.chantelle@gmail.com>
2019-01-21 09:18:52 -0400

    modify downloadData to get msn file

--- downloadData.R
+++ downloadData.R
@@ -81,4 +81,25 @@ downloadData <- function(datadir, glider, mission){
                 destfile = paste(savedir,
                                  kml,
                                  sep=''))
+  # msn file, in directory above data files
+  msavedir <- paste(datadir, glider,'', sep='/')
+  msnpath <- paste(url, 
+                    dirnames, 
+                    glider, 
+                    '', 
+                    sep = '/')
+  mfiles <- getURL(url = msnpath,
+                  ftp.use.epsv = FALSE, dirlistonly = TRUE)
+  mfilenames <- strsplit(mfiles, "\r*\n")[[1]]
+  msn <- mfilenames[grep(pattern = paste0(glider,mission,'.msn'), x = mfilenames)]
+  if(length(msn) != 0){
+    download.file(url = paste(url,
+                              dirnames,
+                              glider,
+                              msn,
+                              sep = '/'),
+                  destfile = paste(savedir,
+                                   msn,
+                                   sep = ''))
+  }
 }
