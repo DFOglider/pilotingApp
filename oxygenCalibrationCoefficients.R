2452d0cf60c39f24fab973a322a8cbca6289c623
melanybelzile <melany.belzile@dfo-mpo.gc.ca>
2019-07-04 12:32:43 -0300

    Added the new calibration coef for SEA019 and SEA022

--- oxygenCalibrationCoefficients.R
+++ oxygenCalibrationCoefficients.R
@@ -1,11 +1,15 @@
 oxycalib <- vector(mode = 'list', length = 5)
 names(oxycalib) <- c('SEA019', 'SEA021', 'SEA022', 'SEA024', 'SEA032')
 
-# R:/Shared/Gliders/SEA019/Calibration_files/SEA019_CTD_DO_calib_files_SN0186.pdf [as of 2018-03-14]
-oxycalib$SEA019 <- c(Soc = 3.2305e-4, Foffset = -830.74, A = -3.4562e-3, B = 1.1709e-4,
+# R:/Shared/Gliders/SEA019/Calibration_files/SEA019_CTD_DO_calib_files_SN0186_2016.pdf [as of 2018-03-14]
+oxycalib$SEA019[[1]] <- c(Soc = 3.2305e-4, Foffset = -830.74, A = -3.4562e-3, B = 1.1709e-4,
                   C = -1.7302e-6, Enom = 0.036)
+# calibration done in December 2018
+# R:/Shared/Gliders/SEA019/Calibration_files/SEA019_CTD_DO_calib_files_SN0186_2018.pdf [as of 2018-03-14]
+oxycalib$SEA019[[2]] <- c(Soc = 2.8304e-4, Foffset = -835.35, A = -3.7649e-3, B = 1.1716e-4,
+                     C = -1.3298-6, Enom = 0.036)
 
-# R:/Shared/Gliders/SEA021/Calibration_files/SEA021_CTD_DO_calib_files_SN0184.pdf [as of 2018-03-14]
+# R:/Shared/Gliders/SEA021/Calibration_files/SEA021_CTD_DO_calib_files_SN0184_2016.pdf [as of 2018-03-14]
 oxycalib$SEA021[[1]] <- c(Soc = 2.7945e-4, Foffset = -794.20, A = -3.4437e-3, B = 1.5480e-4,
                           C = -2.3721e-6, Enom = 0.036)
 # atlshares/MARFIS/Shares/Gliders/SEA021/Calibration_files/SEA021_CTD_DO_calib_files_SN0184_2018 [as of 2018-12-07]
@@ -13,12 +17,15 @@ oxycalib$SEA021[[1]] <- c(Soc = 2.7945e-4, Foffset = -794.20, A = -3.4437e-3, B 
 oxycalib$SEA021[[2]] <- c(Soc = 2.8722e-4, Foffset = -777.23, A = -4.4454e-3, B = 1.8630e-4,
                           C = -2.8650e-6, Enom = 0.036)
 
-
-# R:/Shared/Gliders/SEA022/Calibration_files/SEA022_CTD_DO_calib_files_SN0175.pdf [as of 2018-03-14]
-oxycalib$SEA022 <- c(Soc = 3.1884e-4, Foffset = -807.15, A = -4.2074e-3, B = 2.2413e-4,
+# R:/Shared/Gliders/SEA022/Calibration_files/SEA022_CTD_DO_calib_files_SN0175_2016.pdf [as of 2018-03-14]
+oxycalib$SEA022[[1]] <- c(Soc = 3.1884e-4, Foffset = -807.15, A = -4.2074e-3, B = 2.2413e-4,
                   C = -3.4516e-6, Enom = 0.036)
+# calibration done in March 2019
+# R:/Shared/Gliders/SEA022/Calibration_files/SEA022_DO_calib_files_SN0175_2019.pdf [as of 2018-03-14]
+oxycalib$SEA022[[2]] <- c(Soc = 2.9165e-4, Foffset = -800.49, A = -5.0595e-3, B = 2.3090e-4,
+                     C = -3.2232-6, Enom = 0.036)
 
-# R:/Shared/Gliders/SEA022/Calibration_files/SEA024_CTD_DO_calib_files_SN0188.pdf [as of 2018-03-14]
+# R:/Shared/Gliders/SEA022/Calibration_files/SEA024_CTD_DO_calib_files_SN0188_2016.pdf [as of 2018-03-14]
 oxycalib$SEA024[[1]] <- c(Soc = 2.8277e-4, Foffset = -847.84, A = -2.8377e-3, B = 1.2076e-4,
                           C = -2.0639e-6, Enom = 0.036)
 # atlshares/MARFIS/Shares/Gliders/SEA024/Calibration_files/SEA024_CTD_DO_calib_files_SN0188_2018 [as of 2018-12-07]
